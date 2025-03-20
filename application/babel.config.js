e8bd628d4ab3cc3c5a763e79e1b4cff79d98497c
Ryad <100645955+ryad-all@users.noreply.github.com>
2025-03-20 00:43:07 -0400

    (GH-49) fix: fixed some issues with the packages

--- application/babel.config.js
+++ application/babel.config.js
@@ -1,12 +1,16 @@
-module.exports = {
-    presets: ['module:metro-react-native-babel-preset',"babel-preset-expo"],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
-    ],
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+    ]
   };
+};
